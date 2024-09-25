9337b87d846ac62b3fe98a936be49790b268b042
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2024-09-25 11:34:28 +0530

    Update projectModel.js

--- server/models/projectModel.js
+++ server/models/projectModel.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose')
-
-
 const projectSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
+    title: {
+        type: String,
+        required: true,
     },
-    desc:{
-        type:String,
-        required:true
+    desc: {
+        type: String,
+        required: true
+    },
+    coverimg: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: false,
+    },
+    git: {
+        type: String,
+        required: true,
     }
 })
-
 const Projects = mongoose.model("Projects", projectSchema)
 module.exports = Projects;
