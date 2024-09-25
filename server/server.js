06cd74e8fd97caa0a376acdbccab99823616a1db
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2024-09-25 11:37:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const dbConn = require('./config/db')
 const Projects = require('./routes/projectRoute')
+const Users = require('./routes/userRoute')
 app.use(express.json())
-
 // const dotenv = require('dotenv')
 // dotenv.config()
-
+// const corsOptions = {
+//     origin: process.env.CORSURL
+// };
+app.use(cors())
 const port = process.env.PORT || 7778
 app.use('/projects', Projects)
+app.use('/users', Users)
+
 app.get('/', (req, res) => {
     res.status(400).json("Welcome")
 })
