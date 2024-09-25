47aa9a5dd12d287a92bf1bad0d6ff07151b24a21
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2024-09-25 11:31:51 +0530

    Update projectRoute.js

--- server/routes/projectRoute.js
+++ server/routes/projectRoute.js
@@ -1,27 +1,57 @@
 const express = require('express')
 const router = express.Router();
-const Projects = require('./models/projectModel')
+const Projects = require('../models/projectModel')
 
-router.get('/all' , async(req, res) => {
+router.get('/all', async (req, res) => {
     try {
         const fetchedprojects = await Projects.find()
-        res.json(fetchedprojects).status(200)
-    } catch(error) {
-        res.json(error).status(500)
+        res.status(200).json(fetchedprojects)
+    } catch (error) {
+        res.status(500).json(error)
     }
 })
 
-router.post('./add',async (req,res) => {
-    try{
-        const newprojectdata = await Projects(req.body)
-        const{ title, desc} = newprojectdata
+router.post('/add', async (req, res) => {
+    try {
+        const newprojectdata = new Projects(req.body)
+        const { title, desc } = newprojectdata
         if (!title || !desc) {
-            res.json({ message: "Title & Desc Required"}).status(400)
+            res.status(400).json({ message: "Title & Desc Required" })
+
         }
         const savedata = await newprojectdata.save()
-        res.json(savedata).status(201)
-    } catch(error) {
-        res.json(error).status(500)
+        res.status(200).json(savedata)
+
+    } catch (error) {
+        res.status(500).json(error)
     }
 })
+router.put('/edit/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        // const { id } = req.params;
+        const currentrecord = await Projects.findOne({ _id: id })
+        if (!currentrecord) {
+            res.status(404).json({ message: "Project not found !" })
+        }
+        const updateProject = await Projects.findByIdAndUpdate(id, req.body, { new: true })
+        res.status(200).json(updateProject)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const currentrecord = await Projects.findOne({ _id: id })
+        if (!currentrecord) {
+            res.status(404).json({ message: "Project not found !" })
+        }
+        const deleteProject = await Projects.findByIdAndDelete(id)
+        res.status(200).json({ message: "Project Deleted !" })
+    } catch (e) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router
